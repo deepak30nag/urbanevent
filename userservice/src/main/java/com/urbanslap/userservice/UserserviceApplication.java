a2ef34b6793082a53f8105c5489b2eec3e36c733
deepak30nag <deepucngh@gmail.com>
2021-02-27 23:25:32 +0530

    revert a mistake of changing eureka client to server discovery in user service

--- userservice/src/main/java/com/urbanslap/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/urbanslap/userservice/UserserviceApplication.java
@@ -2,12 +2,12 @@ package com.urbanslap.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
